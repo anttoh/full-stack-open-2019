5b5be08150f3cf70de8143458c96e804932962c6
anttoh <antto.heikura@gmail.com>
2019-05-30 18:39:33 +0300

    another commit

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useState } from "react";
 
-console.log(React.version)
 const Header = ({ course }) => {
   return (
     <div>
---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const sum = good - bad;
+  const amount = good + neutral + bad;
+  const average = sum / amount;
+  const positive = (good / amount) * 100;
+
+  if (amount === 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <div>
+      <h2>statistics</h2>
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={amount} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positivie" value={positive + "%"} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleSetGood = value => setGood(value);
+  const handleSetNeutral = value => setNeutral(value);
+  const handleSetBad = value => setBad(value);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => handleSetGood(good + 1)} text="good" />
+      <Button
+        handleClick={() => handleSetNeutral(neutral + 1)}
+        text="neutral"
+      />
+      <Button handleClick={() => handleSetBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
